c6fe67fe32ee5ca5c65756b3cd69dc6a238739e3
firstock <optimist20@naver.com>
2018-04-11 20:54:51 +0900

    addSolve_aDivB_Java

--- stepbys_fourArith/_1008_aDivB.java
+++ stepbys_fourArith/_1008_aDivB.java
@@ -1,12 +1,36 @@
-import java.util.Scanner;
+//// my
+//import java.util.Scanner;
 //public class Main{
-public class _1008_aDivB{
-	public static void main(String[] arg){
-		Scanner scan= new Scanner(System.in);
-		double a= scan.nextDouble();
-		double b= scan.nextDouble();
-		System.out.println(a/b);
+//public class _1008_aDivB{
+//	public static void main(String[] arg){
+		//Scanner scan= new Scanner(System.in);
+		//double a= scan.nextDouble();
+		//double b= scan.nextDouble();
+		//System.out.println(a/b);
+		//scan.close();
 
-		scan.close();
+
+//	}
+//}
+
+//// other rank 1
+//import java.io.*;
+//import java.util.*;
+//up-down diff: mem_8KB
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _1008_aDivB{
+//public class Main{
+	public static void main(String[] sg) throws Exception{
+		InputStreamReader is= new InputStreamReader(System.in);
+		BufferedReader br= new BufferedReader(is);
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		double a= Double.parseDouble(st.nextToken());
+		double b= Double.parseDouble(st.nextToken());
+		
+		System.out.println(a/b);
 	}
 }
