554d4e695b4cd9a48e66e622bb98bc6f85bc071b
firstock <optimist20@naver.com>
2018-04-11 20:36:15 +0900

    modDirTree_stepbys_io_fourArith

---
+++ stepbys_fourArith/_1008_aDivB.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+//public class Main{
+public class _1008_aDivB{
+	public static void main(String[] arg){
+		Scanner scan= new Scanner(System.in);
+		double a= scan.nextDouble();
+		double b= scan.nextDouble();
+		System.out.println(a/b);
+
+		scan.close();
+	}
+}
