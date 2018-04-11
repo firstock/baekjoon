540533ace19591423c78b76985ee96b641514b70
firstock <optimist20@naver.com>
2018-04-11 13:07:37 +0900

    solve_java_prnDog

---
+++ stepbys/PrnDog.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class PrnDog{
+// local java class== fileName 
+// baekJoon class== Main
+	public static void main(String[] a){
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.print("||_/=\\\\__|");
+	}
+}
